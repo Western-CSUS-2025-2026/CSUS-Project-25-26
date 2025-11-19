e9074da13f5778f0e72f18fc492784b01a447289
Timur Enikeev <163281083+DaymasS@users.noreply.github.com>
2025-11-19 02:41:38 -0500

    Revert "Test PR"

--- interviewer_backend/api/routes/user.py
+++ interviewer_backend/api/routes/user.py
@@ -58,7 +58,6 @@ async def registration_initiate(
         else:
             return StatusResponseModel(status="Success", message=f"Email verification token: {verification_token}")
 
-
 @user.get("/registration/code-verify", response_model=StatusResponseModel)
 async def registration_verify_code(verification_token: int, email: str) -> StatusResponseModel:
     RegistrationVerifyCode(email=email, verification_token=verification_token)
@@ -70,9 +69,8 @@ async def registration_verify_code(verification_token: int, email: str) -> Statu
         or user.verification_token != verification_token
     ):
         raise AuthFailed("Incorrect or expired verification token")
-
+    
     return StatusResponseModel(status="Success", message="Email verified")
-
 
 @user.put("/registration/verify", response_model=StatusResponseModel)
 async def registration_verify(user_data: RegistrationVerify) -> StatusResponseModel:
