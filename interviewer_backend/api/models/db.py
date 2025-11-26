caaa2f0d856ee763b84e39b1df3332fe87c428b3
Timur Enikeev <enikeevt04@gmail.com>
2025-11-26 13:17:04 -0500

    Revert "Merge branch 'david-backend' of https://github.com/Western-CSUS-2025-2026/CSUS-Project-25-26 into david-backend"
    This reverts commit 92903e65d77123ff4bb61fa4e3634e4302f71f07, reversing
    changes made to 0dfb649bf5697e8733dfe308ddc9452f52b644d9.

--- interviewer_backend/api/models/db.py
+++ interviewer_backend/api/models/db.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import datetime
+import enum
 import logging
 
-from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -28,6 +29,9 @@ class User(BaseDbModel):
     )
     sessions: Mapped[list[UserSession]] = relationship(
         "UserSession", foreign_keys="UserSession.user_id", back_populates="user", cascade='all, delete'
+    )
+    interview_sessions: Mapped[list["Session"]] = relationship(
+        "Session", foreign_keys="Session.user_id", cascade='all, delete'
     )
 
 
