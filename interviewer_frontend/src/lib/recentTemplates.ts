d0675b88e1c3523fceaaf44a2763aebb0374a27d
LucasVand <125316573+LucasVand@users.noreply.github.com>
2025-11-28 18:46:33 -0500

    Revert "Saif recent session bar"

--- interviewer_frontend/src/lib/recentTemplates.ts
+++
@@ -1,9 +0,0 @@
-import { defaultTemplate, Template } from "@/types/template";
-import { sleep } from "./sleep";
-
-export async function getRecentTemplates(): Promise<Template[]> {
-  // will have a network request eventually
-
-  await sleep(1000);
-  return [defaultTemplate, defaultTemplate];
-}
