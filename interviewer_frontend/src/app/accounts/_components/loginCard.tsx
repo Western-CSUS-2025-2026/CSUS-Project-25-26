fdda92e8b4d50806cd445de52fa53ba1cd3f4a11
slushie23 <sahota.yuvraj123@gmail.com>
2026-01-23 15:51:24 -0500

    Added the CreateAccountCard and buttons

--- interviewer_frontend/src/app/accounts/_components/loginCard.tsx
+++ interviewer_frontend/src/app/accounts/_components/loginCard.tsx
@@ -1,39 +1,65 @@
 import Card from "@/components/card/card";
 import styles from "./loginCard.module.css";
 
-export default function LoginCard() {
-  return (
-    <div className={styles.container}>
-        <Card>
-        <h1 className={styles.header}>Login</h1>
-        <div className={styles.coloumn}>
-        <div className={styles.line}></div>
 
-        <div className={styles.infoBox}>
-        <p>Email</p>
-        <input
-        type="text"
-        className={styles.textBox}
-        placeholder="Enter your email"
-        />
+interface LoginCardProps {
+  onSignUp: () => void;
+}
 
-        <p>Password</p>
-        <input
-        type="password"
-        className={styles.textBox}
-        placeholder="Enter your password"
-        />
+export default function LoginCard({ onSignUp }: LoginCardProps) {
+  return (    
+    <>
+      <div className={styles.container}>
+        {/* wrapper class */}
+        <div className="loginCardRadiusOverride">
+        <div className={styles.sizeBox}>
+          <Card fillHeight fillWidth>
+            <h1 className={styles.header}>Login</h1>
+
+            <div className={styles.coloumn}>
+              <div className={styles.line}></div>
+
+              <div className={styles.infoBox}>
+                <p>Email</p>
+                <input
+                  type="text"
+                  className={styles.textBox}
+                  placeholder="Enter your email"
+                  autoComplete="email"
+                />
+
+                <p>Password</p>
+                <input
+                  type="password"
+                  className={styles.textBox}
+                  placeholder="Enter your password"
+                  autoComplete="current-password"
+                />
+              </div>
+
+              <button className={styles.loginButton} type="button">Login</button>
+
+            <div className={styles.linkText}>
+            <button type="button" className={styles.linkButton}>
+                Forgot Password
+            </button>
+           <button type="button" onClick={onSignUp} className={styles.linkButton}>
+                Sign Up
+            </button>
+            </div>
+
+            </div>
+          </Card>
         </div>
+      </div>
+      </div>
 
-        <div className={styles.loginButton}>Login</div>
-
-        <div className={styles.linkText}>
-            <p>Forgot Password</p>
-            <p>Sign Up</p>
-        </div>
-
-        </div>
-    </Card>
-    </div>
+      {/*change card border radius*/}
+      <style>{`
+        .loginCardRadiusOverride .cardStyle {
+          border-radius: 2em !important;
+        }
+      `}</style>
+    </>
   );
 }
