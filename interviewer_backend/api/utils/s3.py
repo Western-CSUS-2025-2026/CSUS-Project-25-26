5789dcc5b0cdc2746721a818126055ac8319fddc
Timur Enikeev <enikeevt04@gmail.com>
2026-03-03 15:57:09 -0500

    Change S3 client version

--- interviewer_backend/api/utils/s3.py
+++ interviewer_backend/api/utils/s3.py
@@ -2,6 +2,7 @@ import uuid
 from functools import lru_cache
 
 import boto3
+from botocore.config import Config
 
 from api.settings import get_settings
 
@@ -16,6 +17,7 @@ def get_s3_client():
         region_name=settings.AWS_REGION,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        config=Config(signature_version="s3v4"),
     )
 
 
