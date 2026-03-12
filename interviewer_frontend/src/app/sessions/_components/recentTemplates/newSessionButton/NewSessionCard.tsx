e545d2cc65934e5fe293da878a7343b35e83d711
Lucas <lucasvanderwielen@icloud.com>
2026-03-12 15:59:49 -0400

    changed new session model to use nextjs router

--- interviewer_frontend/src/app/sessions/_components/recentTemplates/newSessionButton/NewSessionCard.tsx
+++ interviewer_frontend/src/app/sessions/_components/recentTemplates/newSessionButton/NewSessionCard.tsx
@@ -2,25 +2,31 @@
 
 import InvertedPlusIcon from "@/components/icons/invertedPlusIcon";
 import styles from "./newSessionCard.module.css";
+import { useRouter } from "next/navigation";
 
 export default function NewSessionCard() {
+  const router = useRouter();
   function openModal() {
     const params = new URLSearchParams(window.location.search);
     params.set("newSession", "1");
     params.set("step", "select");
     params.delete("templateId");
 
-    window.history.pushState({}, "", `/sessions?${params.toString()}`);
-
-    // Force client components to react immediately
-    window.dispatchEvent(new Event("urlchange"));
+    router.push(`/sessions?${params.toString()}`);
   }
 
   return (
-    <div className={styles.button} onClick={openModal} role="button" tabIndex={0}>
+    <div
+      className={styles.button}
+      onClick={openModal}
+      role="button"
+      tabIndex={0}
+    >
       <div className={styles.labelGroup}>
         <h3 className={styles.title}>New Session</h3>
-        <p className={styles.subtitle}>Start a new session with a curated template</p>
+        <p className={styles.subtitle}>
+          Start a new session with a curated template
+        </p>
       </div>
 
       <InvertedPlusIcon
